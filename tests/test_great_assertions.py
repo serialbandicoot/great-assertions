d079fd0a35d5582372d364bfe5dea4e7710d4b96
Sam Treweek <samtreweek@gmail.com>
2021-09-17 16:07:22 +0100

    add real tests

--- tests/test_great_assertions.py
+++ tests/test_great_assertions.py
@@ -1,6 +1,20 @@
 import unittest
+import sys
+import pytest
 
-class GreatAssertionTests(unittest.TestCase):
-    
-    def test_something(self):
-        self.assertTrue(True)
+sys.path.insert(0, "../great-assertion/src")
+from unittest.case import TestCase
+from great_assertions import GreatAssertions
+import pandas as pd
+
+
+class GreatAssertionTests(GreatAssertions):
+    def test_expect_table_row_count_to_equal(self):
+        df = pd.DataFrame({"calories": [420, 380, 390], "duration": [50, 40, 45]})
+        self.assertExpectTableRowCountToEqual(df, 3)
+
+    def test_raises_type_error(self):
+        with pytest.raises(AssertionError) as excinfo:
+            self.assertExpectTableRowCountToEqual(1, 1)
+
+        assert "Object is not type DataFrame" in str(excinfo.value)
