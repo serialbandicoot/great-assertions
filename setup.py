5d2012885d753cdecc874419fcbe68962b4464bf
Sam Treweek <samtreweek@gmail.com>
2021-10-01 09:30:16 +0100

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="great-assertions",
-    version="0.0.21",
+    version="0.0.22",
     description="Inspired by the library great-expectations",
     long_description=README,
     long_description_content_type="text/markdown",
