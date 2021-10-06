4ac86857047eab1861b8f1e2ace7f0f2a057bd5c
Sam Treweek <samtreweek@gmail.com>
2021-10-06 16:48:00 +0100

    incorrect argument ref

--- tests/test_great_assertions_py_spark.py
+++ tests/test_great_assertions_py_spark.py
@@ -237,7 +237,7 @@ class GreatAssertionPySparkTests(GreatAssertions):
             [{"col_1": "2019/05/13"}, {"col_1": "2018/12/12"}, {"col_1": "2015/10/01"},]
         )
         self.assertExpectDateRangeToBeLessThan(
-            df, "col_1", "2019/05/14", format="%Y/%m/%d"
+            df, "col_1", "2019/05/14", date_format="%Y/%m/%d"
         )
 
     def test_assert_expect_date_range_to_be_less_than_default(self):
